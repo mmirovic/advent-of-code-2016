1c7d1ac124c35d14d1d557da952f88453c11fdbc
Milivoje Mirovic <mmirovic@gmail.com>
2017-01-19 20:23:23 +0100

    Day 11: Radioisotope Thermoelectric Generators

---
+++ puzzle11/puzzle11.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# Advent of code 2016 by mmirovic
+
+f = open('input', 'r')
+		
+floor = []
+steps = 0
+part = 2
+
+for l in f:
+	el = 0
+	l = l.split(' a ')
+	for w in l:
+		if 'generator' in w or 'microchip' in w:
+			el += 1
+	floor += [el]
+
+if part == 2:
+	floor[0] += 4
+
+while sum(floor) != floor[-1]:
+	for f in range(4-1):
+		if floor[f] != 0:
+			steps += 1 + (floor[f]-2)*2
+			floor[f+1] += floor[f]
+			floor[f] = 0
+
+print floor, steps
